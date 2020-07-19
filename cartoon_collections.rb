d2be92c15d4b3e97c89f0ff60d5dc5a1ecf7b60c
Shelley Brook <sbrook13@gmail.com>
2020-07-19 01:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|call| call == valid_calls.any?}
+  planeteer_calls.find {|call| valid_calls.include? call}
 end
