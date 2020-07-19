1920adc87315ced059ac53310cca91dacf6ac1db
Shelley Brook <sbrook13@gmail.com>
2020-07-19 00:16:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  array.map do |element| element.capitalize
+  array.map {|element| element.capitalize}
 end
 
 def long_planeteer_calls(planeteer_calls)
